2f50f52a4ab2a0a34877dfe342c39a7364ca8813
vladbatalan <batabtro@gmail.com>
2022-04-17 12:45:05 +0300

    Database file added

---
+++ OtpServer/src/test/java/com/otp/otpserver/OtpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otp.otpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
